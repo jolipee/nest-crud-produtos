c12fcac0568b5ad9123b2dbe1c661fc019b3de42
Louie Nery <louie.nery1500@gmail.com>
2025-05-13 23:17:18 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProdutosModule } from './produtos/produtos.module';
+ import { TypeOrmModule } from '@nestjs/typeorm';
+ import { ProdutosModule } from './produtos/produtos.module';
 
-@Module({
-  imports: [ProdutosModule],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
+ @Module({
+   imports: [
+     TypeOrmModule.forRoot({
+       type: 'sqlite',
+       database: 'db.sqlite',
+       entities: [__dirname + '/**/*.entity{.ts,.js}'],
+       synchronize: true,
+     }),
+     ProdutosModule
+   ],
+ })
+ export class AppModule {}
