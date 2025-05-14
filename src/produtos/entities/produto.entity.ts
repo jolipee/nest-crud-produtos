1c0bbb32edb7b875a625079e0fa0704be6bbf952
Louie Nery <louie.nery1500@gmail.com>
2025-05-13 23:18:07 -0300

    Update produto.entity.ts

--- src/produtos/entities/produto.entity.ts
+++ src/produtos/entities/produto.entity.ts
@@ -1,1 +1,23 @@
-export class Produto {}
+ /* eslint-disable @typescript-eslint/no-unsafe-call */
+ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+ 
+ @Entity()
+ export class Produto {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    nome: string;
+
+    @Column({ type: 'decimal', precision: 10, scale: 2 })
+    preco: number;
+
+    @Column()
+    emailEmpresa: string;
+
+    @Column()
+    dataValidade: Date;
+    
+    @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
+    createdAt: Date;
+ }
