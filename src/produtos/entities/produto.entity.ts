78083604b6d09013773bc7553415b83860cb67b2
Matucaul <mateushenrique3056@outlook.com>
2025-05-14 21:19:51 -0300

    Update produto.entity.ts

--- src/produtos/entities/produto.entity.ts
+++ src/produtos/entities/produto.entity.ts
@@ -1,23 +1,33 @@
- /* eslint-disable @typescript-eslint/no-unsafe-call */
- import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
- 
- @Entity()
- export class Produto {
-    @PrimaryGeneratedColumn()
-    id: number;
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { Categoria } from 'src/categorias/entities/categoria.entity';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
 
-    @Column()
-    nome: string;
+@Entity()
+export class Produto {
+@PrimaryGeneratedColumn()
+id: number;
 
-    @Column({ type: 'decimal', precision: 10, scale: 2 })
-    preco: number;
+@Column()
+nome: string;
 
-    @Column()
-    emailEmpresa: string;
+@Column({ type: 'decimal', precision: 10, scale: 2 })
+preco: number;
 
-    @Column()
-    dataValidade: Date;
-    
-    @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
-    createdAt: Date;
- }
+@Column()
+emailEmpresa: string;
+
+@Column()
+ dataValidade: Date;
+
+@Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
+ createdAt: Date;
+
+@ManyToOne(() => Categoria, (categoria) => categoria.produtos, { nullable: false })
+
+@JoinColumn({ name: 'categoriaId' })
+categoria: Categoria;
+
+@Column()
+categoriaId: number;
+}
